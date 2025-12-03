d04240c702ffce55391b763699535293d72b6359
safaacht <safaachtaoui@gmail.com>
2025-12-03 16:48:05 +0100

    initialisation + creation de header et footer

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini LMS - Gestion des Cours</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- <script src="script.js" defer></script> -->
+</head>
+<body>
+    <header>
+        <h1>AAAAA</h1>
+        <nav>
+            <ul>
+                <li><a href="courses_list.php">Cours</a></li>
+                <li><a href="sections_list.php">Sections</a></li>
+                <li><a href="add_course.php">Ajouter(cours)</a></li>
+                <li><a href="add_section.php">Ajouter(section)</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ index_php.php
@@ -0,0 +1,12 @@
+<?php
+include('connect_php.php');
+?>
+
+<?php include('header.php') ?>
+
+    <main>
+       
+    </main>
+
+   <?php include('./footer.php') ?>
+
