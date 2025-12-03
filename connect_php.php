050c99fc3b7de6d59f7003cbb5382407072f4d43
safaacht <safaachtaoui@gmail.com>
2025-12-03 16:50:22 +0100

    connection entre php et sql

---
+++ connect_php.php
@@ -0,0 +1,23 @@
+<?php
+
+$db_server="localhost";
+$db_user="root";
+$db_mdp="";
+$db_name="gestion_cours";
+$connect="";
+
+$connect=mysqli_connect($db_server,
+                        $db_user,
+                        $db_mdp,
+                        $db_name
+);
+
+
+// making sure if the connection went well
+if($connect){
+    echo "You are connected!";
+}else{
+    echo"Connection failed!";
+}
+
+?>
