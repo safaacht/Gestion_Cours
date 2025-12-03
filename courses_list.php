8dea8566ee6c5f0c4e658abe9ab04c4bdb738e63
safaacht <safaachtaoui@gmail.com>
2025-12-03 16:51:34 +0100

    initialisation des fichiers

---
+++ courses_list.php
@@ -0,0 +1,31 @@
+<?php
+// checking if the data is sent to the server
+// if(isset($_GET['submit'])){
+//     echo $_GET['title'];
+//     echo $_GET['level'];
+
+// }
+// if(isset($_POST['submit'])){
+//     echo $_POST['title'];
+//     echo $_POST['level'];
+// }
+
+// if(isset($_POST['submit'])){
+//     session_start();
+
+//     $_SESSION['title']=$_POST['title'];
+//     echo $_SESSION['title'];
+
+//     if(!isset($_SESSION['counter'])){
+//         $_SESSION['counter']=0;
+//     }else{
+//         $_SESSION['counter']++;
+//     }
+// echo $_SESSION['counter'];
+// }
+
+
+?>
+
+<?php include('header.php') ?>
+<?php include('./footer.php') ?>
