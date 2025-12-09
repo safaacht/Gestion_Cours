430b5506c2e8640510c9c0f1d95d9400e7f38e86
safaacht <safaachtaoui@gmail.com>
2025-12-10 00:30:22 +0100

    adding DELETE functionality

--- courses_delete.php
+++ courses_delete.php
@@ -1,10 +1,23 @@
 <?php
-include('connect_php.php');
+require('connect_php.php');
 
-if(isset($_GET['delete'])){
-    $id=$_GET['delete'];
+if(isset($_GET['id'])){
+    $id=$_GET['id'];
+    // $id = mysqli_real_escape_string($connect, $id);
+    $id = (int)$id;
 
     $del="DELETE FROM courses WHERE id=$id";
-    mysqli_query($connect,$del);
-}
+
+    if(mysqli_query($connect,$del)){
+        header("Location:courses_list.php");
+        exit();
+    }else{
+        echo "Erreur lors de la suppression du cours: " . mysqli_error($connect);
+    }
+}else{
+        // get is done without id
+        header("Location:courses_list.php");
+        exit();
+    }
+    
 ?>
