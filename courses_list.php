c522f30a6bfee57c1bcae89fc07fcb361d7ea84f
safaacht <safaachtaoui@gmail.com>
2025-12-10 00:35:05 +0100

    Update: buttons to href

--- courses_list.php
+++ courses_list.php
@@ -1,19 +1,14 @@
-<?php include('header.php') ?>
-
-
 <?php
-$conn=require('connect_php.php');
+include('header.php');
+require('connect_php.php');
 
 $sql="SELECT * FROM courses";
-$result=mysqli_query($conn,$sql);
+$result=mysqli_query($connect,$sql);
 
 // fetching the row results to an array
 $courses=mysqli_fetch_all($result,MYSQLI_ASSOC);
 // print_r($courses);
-
 ?>
-
-<?php include('courses_delete') ?>
 
 <div class="all_cards">
     <?php foreach($courses as $course){ ?>
@@ -25,7 +20,12 @@ $courses=mysqli_fetch_all($result,MYSQLI_ASSOC);
                 <h5> <?php echo htmlspecialchars($course['created_at']);?> </h5>
     
             </div>
-            <button class="delete">Delete</button>
+
+            <a class="btn-secondary btn" href="add_section.php?course_id=<?= $course['id'] ?>">Sections</a>
+            <a class="btn" href="courses_edit.php?id=<?= $course['id'] ?>">Edit</a>
+            <a class="btn-danger btn" onclick="return confirm('Supprimer ce cours ?')" 
+               href="courses_delete.php?id=<?= $course['id'] ?>">DELETE</a>
+
         </div>
     <?php }?>
 
