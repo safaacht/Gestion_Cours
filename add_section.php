b429e475dac4d2ee21df977aaa77dfff697cd6ae
safaacht <safaachtaoui@gmail.com>
2025-12-10 02:05:53 +0100

    adding section

--- add_section.php
+++ add_section.php
@@ -1,18 +1,31 @@
-<?php include('header.php') ?>
+<?php 
+include('header.php');
+require 'connect_php.php';
+
+$cid=$_GET['course_id'];
+if($_POST){
+$title=$_POST['title'];
+$content=$_POST['content']; 
+$position=$_POST['position'];
+
+
+mysqli_query($connect,"INSERT INTO sections(course_id,title,content,position,created_at) VALUES($cid,'$title','$content',$pos,NOW())");
+
+header("Location: sections_by_course.php?course_id=$cid"); 
+exit;
+}
+?>
+
 
 <section>
     <form id="my-form" action="add_course.php" method="POST">
     <h4>ADD Section</h4>
     <label>Title:</label>
     <input type="text" name="title">
-    <label>To course:</label>
-    <input type="number" name="course_id">
     <label>Content:</label>
     <input type="text" name="content">
     <label>Position:</label>
     <input type="number" name="position">
-    <label>Created at:</label>
-    <input type="datetime-local" name="created_at">
 
     <button id="submit" name="submit">Submit</button>
     </form>
