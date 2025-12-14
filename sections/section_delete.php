8a10648be689b327f892f135fad6cb5d0cd7d0d4
safaacht <safaachtaoui@gmail.com>
2025-12-14 20:56:28 +0100

    updating the code structure by adding conditions

--- sections/section_delete.php
+++ sections/section_delete.php
@@ -1,10 +1,18 @@
 <?php
-require 'connect_php.php';
+include 'connect_php.php';
 
-$id=$_GET['id']; 
-$cid=$_GET['course_id'];
+if(isset($_GET['id'])){
+    $id=$_GET['id'];
+    $cid=$_GET['course_id'];
 
-mysqli_query($connect,"DELETE FROM sections WHERE id=$id");
-
-header("Location: sections_by_course.php?course_id=$cid"); 
+if(mysqli_query($connect,"DELETE FROM sections WHERE id=$id")){
+    header("Location: sections_list.php?course_id=$cid"); 
+    exit();
+}else{
+    echo "Erreur lors de la suppression du section: " . mysqli_error($connect);
+}
+}else{
+    echo "Error 404: Section not found!";
+    exit();
+}
 ?>
